15aa4c8cedec20834061a82a748b0374969ba869
Kim-woo-gyeong <lovelykwg@naver.com>
2020-03-20 15:50:15 +0900

    Validation update

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.douzone.mysite.controller;
 
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,12 +25,22 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping(value="/join",method=RequestMethod.GET)
-	public String join() {
+	public String join(@ModelAttribute UserVo vo) {
 		return "user/join";
 	}
 	
 	@RequestMapping(value="/join",method=RequestMethod.POST)
-	public String join(UserVo vo) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error:list) {
+//				System.out.println(error);
+//			}
+			model.addAllAttributes(result.getModel());
+			//어떤 필드에서 에러가 났는지, 메시지 출력. 모든 에러 내용을 jsp로 돌림
+			return "user/join";
+		}
 		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
